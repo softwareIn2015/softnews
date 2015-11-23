281e33beeb1aaa32b3da0183fe0bc80e03950819
Raitoxz <2560804242@qq.com>
2015-11-23 11:07:34 +0800

    修改了views

--- news/models.py
+++ news/models.py
@@ -1,16 +1,31 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
 
 # Create your models here.
 class UserModel(models.Model):
-    Name = models.CharField(max_length=40, primary_key=True)#20个字符
+    Name = models.CharField(max_length=40, primary_key=True)
     Email = models.EmailField()
-    Password = models.CharField(max_length=20)#密码最长为20个字符
+    Password = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return self.Name
+
+
+class UrlsModel(models.Model):
+    Source = models.CharField(max_length=1000)
+    Urls = models.CharField(max_length=10000)
+
+    def __unicode__(self):
+        return u'%s %s' % (self.Source, self.Urls)
 
 
 class CommentsModel(models.Model):
-    Text = models.TextField(verbose_name=u'评论内容')
+    Text = models.TextField(verbose_name=u'璇勮鍐呭')
     User = models.ForeignKey(UserModel)
+
+    def __unicode__(self):
+        return self.Text
 
 
 class NewsModel(models.Model):
@@ -19,7 +34,5 @@ class NewsModel(models.Model):
     Summary = models.TextField()
     Comments = models.ForeignKey(CommentsModel)
 
-
-class UrlsModel(models.Model):
-    Source = models.CharField(max_length=1000)
-    Urls = models.CharField(max_length=10000)
+    def __unicode__(self):
+        return self.Title
--- news/views.py
+++ news/views.py
@@ -7,3 +7,51 @@ from models import *
 
 def homepage(request):
     return render_to_response('home.html')
+
+
+def news_home(request):
+    return render_to_response("home.html")
+
+
+def hot_news(request):
+    return render_to_response("hot.html")
+
+
+def social_news(request):
+    return render_to_response("social.html")
+
+
+def amusement_news(request):
+    return render_to_response("amusement.html")
+
+
+def international_news(request):
+    return render_to_response("international.html")
+
+
+def domestic_news(request):
+    return render_to_response("domestic.html")
+
+
+def sports_news(request):
+    return render_to_response("sports.html")
+
+
+def military_news(request):
+    return render_to_response("military.html")
+
+
+def contact_news(request):
+    return render_to_response("contact.html")
+
+
+def detail_news(request):
+    return render_to_response("detail.html")
+
+
+def login(request):
+    return render_to_response("login.html")
+
+
+def register(request):
+    return render_to_response("register.html")
