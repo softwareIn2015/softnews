c224a5a213b6b5c3ba0843863ad039791e3e2fee
greenmjy <mr_lovegreen@163.com>
2015-11-28 10:13:07 +0800

    修改后台管理，可以进入后台轻松管理数据库

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from news.models import *
 
+admin.site.register(UserModel)
+admin.site.register(UrlsModel)
+admin.site.register(CommentsModel)
+admin.site.register(NewsModel)
 # Register your models here.
+
+
