2e0935c36ce05f25ac1a080fad1d6b33d3032c62
greenmjy <mr_lovegreen@163.com>
2015-12-23 10:22:14 +0800

    完善了一些进入新闻详情页的链接

--- softnews/settings.py
+++ softnews/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'newsdb0',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': '24678',                  # Not used with sqlite3.
+        'PASSWORD': 'mjy123456',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
