830461303807145a95d2028d2c58a504ff0359d0
Raitoxz <2560804242@qq.com>
2015-12-23 19:54:28 +0800

    更新了爬虫
    能够爬到新浪、网易的新闻

--- Bug.py
+++ Bug.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""__author__ = 'Xiang'"""
+# __author__ = 'Xiang'
 import urllib
 from bs4 import BeautifulSoup as bs
 import MySQLdb
@@ -10,68 +10,247 @@ def get_html(my_url):
     my_html = page.read()
     return my_html
 
-url = 'http://tuijian.hao123.com/'
-html = get_html(url)
-soup = bs(html, 'lxml', from_encoding="utf-8")
 
-try:
-    conn = MySQLdb.connect(host='localhost', user='root', passwd='mjy123456', port=3306)
-    cur = conn.cursor()
-    conn.select_db('newsdb')
-    for link in set(soup.find_all('a')):
-        Title = unicode(link.string).strip().replace(u'\xa0', '').encode('gbk')#\xa0是HTML里的&nbsp
-        print Title
-        Urls = unicode(link.get('href')).encode('utf-8')
-        if len(Title) <= 8 or Urls == 'http://news.sina.com.cn/' or Urls == 'javascript:' or Urls is None \
-                or Urls == 'javascript:;' or Urls == 'Prev' or Urls == 'Next' or Urls == 'Left' or Urls == 'Right' \
-                or Urls == 'http://data.huanqiu.com/' or Urls == 'http://humor.huanqiu.com/':
-            continue
+def delete(cur):
+    # 删除数据
+    cur.execute('SET foreign_key_checks=0')
+    cur.execute('truncate table news_urlsmodel')
+    cur.execute('truncate table news_newsmodel')
+    cur.execute('SET foreign_key_checks=1')
 
-        Source = 'sina'
-        Classification = 'tech'
-        Title = Title.decode('gbk').encode('utf-8')#存储的时候采用utf-8
 
-        #插入url表中
-        url_value = [Source, Urls]
+def show_data(cur):
+    # 查看数据库中的数据
+    cur.execute('select * from news_urlsmodel')
+    urls = cur.fetchall()
+    for url in urls:
+        print url
+
+    cur.execute('select * from news_newsmodel')
+    news = cur.fetchall()
+    for new in news:
+        print new
+
+
+class Bugger:
+    def __init__(self, url_=None, classfication_=None, source_=None, news_class_=None, image_class_=None):
+        self.url = url_
         try:
-            cur.execute('insert into news_urlsmodel(Source, Urls) values (%s,%s)', url_value)
-        except UnicodeEncodeError:
-            continue
-        conn.commit()
-
-        #根据得到的url爬取新闻的概要
-        detail_url = Urls
-        try:
-            detail_html = get_html(detail_url)
+            self.html = get_html(self.url)
         except IOError:
-            continue
-        detail_soup = bs(detail_html, 'lxml', from_encoding="utf-8")
+            return
+        # print self.html
+        self.soup = bs(self.html, 'lxml', from_encoding="utf-8")
+        # print self.soup.original_encoding
+        self.classification = classfication_
+        self.source = source_
+        self.news_class = news_class_
+        self.image_class = image_class_
+        if self.source == 'sina':
+            self.scripy_sina()
+        elif self.source == '163':
+            self.scripy_163()
+
+    def scripy_sina(self):
         try:
-            Summary = unicode(detail_soup.p.string).strip().encode('utf-8')
-        except AttributeError:
-            continue
+            conn = MySQLdb.connect(host='localhost', user='root', passwd='24678', port=3306)
+            cur = conn.cursor()
+            conn.select_db('newsdb')
 
-        #插入news表中
-        url = cur.execute('select * from news_urlsmodel where urls=%s', Urls)
-        news_value = [Title, url, Classification, Summary]
-        cur.execute('insert into news_newsmodel(Title, url_id, Classification, Summary) values (%s,%s,%s,%s)', news_value)
-        conn.commit()
+            # 爬取网页信息
+            news = self.soup.find('div', class_=self.news_class)
 
-    # cur.execute("select * from news_newsmodel where classification='tech'")
-    # titles = cur.fetchall()
-    # for title in titles:
-    #     print title
-    # print "delete from news_urlsmodel where urls='%s'" % 'http://humor.huanqiu.com/'
-    # cur.execute('select * from news_urlsmodel')
-    # urls = cur.fetchall()
-    # for url in urls:
-    #     if url[2] == 'http://data.huanqiu.com/':
-    #         print url[2]
-    cur.close()
-    conn.close()
+            for div in news.find_all('div'):
+                for link in div.find_all('a'):
+                    try:
+                        if len(link.string) == 0:  # 标题为空的不要存
+                            continue
+                    except TypeError:
+                        continue
 
-except MySQLdb.Error, e:
-    try:
-        sqlError = "Error %d:%s" % (e.args[0], e.args[1])
-    except IndexError:
-        print "MySQL Error:%s" % str(e)
+                    # 获取标题和链接
+                    title = unicode(link.string).strip().encode('utf-8')  # .replace(u'\xa0', '')  # \xa0是HTML里的&nbsp
+                    urls = unicode(link.get('href')).strip().encode('utf-8')
+
+                    # 根据得到的url爬取新闻的概要,如果能爬到新闻的概要才存到数据库中
+                    detail_url = urls
+                    try:
+                        detail_html = get_html(detail_url)
+                    except IOError:
+                        continue
+
+                    detail_soup = bs(detail_html, 'lxml', from_encoding="utf-8")
+                    detail_news = detail_soup.find_all('div', id='artibody')
+
+                    # 寻找摘要
+                    flag = 0
+                    for detail_div in detail_news:
+                        for detail_p in detail_div.find_all('p'):
+                            p = detail_p.string
+                            try:
+                                if len(p) >= 20:
+                                    summary = p
+                                    flag = 1  # flag等于1表示存在满足条件的摘要，跳出循环
+                                    break
+                            except TypeError:
+                                continue
+                        if flag == 1:
+                            break
+                    if flag == 0:  # flag等于0表示没有满足条件的p
+                        continue
+
+                    try:
+                        summary = unicode(summary).strip().encode('utf-8')  # .replace(u'\xa0', '')
+                    except AttributeError:  # 没有文章的网站不存
+                        continue
+
+                    # 寻找图片
+                    try:
+                        image = detail_news.find(class_=self.image_class).img['src']
+                    except AttributeError:  # 该新闻没有图片就不存，空格表示没有图片
+                        image = ' '
+
+                    cur.execute('SET NAMES utf8;')
+                    # 插入news表中
+                    likes = 0
+                    news_value = [title, self.classification, summary, image, likes]
+                    cur.execute('insert into news_newsmodel(Title, Classification, Summary, Image, Likes) \
+                                    values (%s,%s,%s,%s,%s)', news_value)
+
+                    # 插入url表中
+                    cur.execute('select * from news_newsmodel where title = %s', title)
+                    news_id = cur.fetchone()
+                    url_value = [self.source, urls, news_id[0]]
+                    cur.execute('insert into news_urlsmodel(Source, Urls, News_id) values (%s,%s,%s)', url_value)
+                    conn.commit()
+
+            # # 删除数据
+            # delete(self.cur)
+
+            cur.close()
+            conn.close()
+
+        except MySQLdb.Error, e:
+            try:
+                print "Error %d:%s" % (e.args[0], e.args[1])
+            except IndexError:
+                print "MySQL Error:%s" % str(e)
+
+    def scripy_163(self):
+        try:
+            conn = MySQLdb.connect(host='localhost', user='root', passwd='24678', port=3306)
+            cur = conn.cursor()
+            conn.select_db('newsdb')
+
+            # 爬取网页信息
+            news = self.soup.find('div', class_=self.news_class)
+
+            for link in news.find_all('a'):
+                # link = div.a
+                try:
+                    if len(link.string) == 0:  # 标题为空的不要存
+                        continue
+                except TypeError:
+                    continue
+
+                # 获取标题和链接
+                title = unicode(link.string).strip().encode('utf-8')  # .replace(u'\xa0', '')  # \xa0是HTML里的&nbsp
+                urls = unicode(link.get('href')).strip().encode('utf-8')
+
+                # 根据得到的url爬取新闻的概要,如果能爬到新闻的概要才存到数据库中
+                detail_url = urls
+                try:
+                    detail_html = get_html(detail_url)
+                except IOError:
+                    continue
+
+                detail_soup = bs(detail_html, 'lxml', from_encoding="utf-8")
+                try:
+                    detail_news = detail_soup.find_all('div', class_='end-text')
+                    if len(detail_news) == 0:
+                        detail_news = detail_soup.find_all('div', class_='endText')
+                    if len(detail_news) == 0:
+                        continue
+                except AttributeError:  # 防止爬到图集，不好处理
+                    continue
+
+                # 寻找摘要
+                flag = 0
+                for detail_div in detail_news:
+                    for detail_p in detail_div.find_all('p'):
+                        p = detail_p.string
+                        try:
+                            if len(p) >= 20:
+                                summary = p
+                                flag = 1  # flag等于1表示存在满足条件的摘要，跳出循环
+                                break
+                        except TypeError:
+                            continue
+                    if flag == 1:
+                        break
+                if flag == 0:  # flag等于0表示没有满足条件的p
+                    continue
+
+                try:
+                    summary = unicode(summary).strip().encode('utf-8')  # .replace(u'\xa0', '')
+                except AttributeError:  # 没有文章的网站不存
+                    continue
+
+                # 寻找图片
+                if len(detail_news[0].find_all('p', class_=self.image_class)) == 0:  # 没有图片
+                    image = ' '
+                else:
+                    try:
+                        image = detail_news[0].find_all('p', class_=self.image_class)[0].img['src']
+                    except TypeError:
+                        image = ' '
+
+                cur.execute('SET NAMES utf8;')
+                # 插入news表中
+                likes = 0
+                news_value = [title, self.classification, summary, image, likes]
+                cur.execute('insert into news_newsmodel(Title, Classification, Summary, Image, Likes) \
+                                values (%s,%s,%s,%s,%s)', news_value)
+
+                # 插入url表中
+                cur.execute('select * from news_newsmodel where title = %s', title)
+                news_id = cur.fetchone()
+                url_value = [self.source, urls, news_id[0]]
+                cur.execute('insert into news_urlsmodel(Source, Urls, News_id) values (%s,%s,%s)', url_value)
+                conn.commit()
+
+            # # 删除数据
+            # delete(cur)
+
+            cur.close()
+            conn.close()
+
+        except MySQLdb.Error, e:
+            try:
+                print "Error %d:%s" % (e.args[0], e.args[1])
+            except IndexError:
+                print "MySQL Error:%s" % str(e)
+
+
+if __name__ == '__main__':
+    # news_urls_sina = ['http://news.sina.com.cn/society/', 'http://news.sina.com.cn/china/',
+    #                   'http://news.sina.com.cn/world/','http://mil.news.sina.com.cn/', 'http://sports.sina.com.cn/',
+    #                   'http://ent.sina.com.cn/','http://news.sina.com.cn/']  # 新闻链接
+    # classfications_sina = ['social', 'domestic', 'international', 'military', 'sports', 'amusement', 'hot']  # 分类
+    # sources_sina = ['sina', 'sina', 'sina', 'sina', 'sina', 'sina', 'sina']  # 新闻来源
+    # news_class_sina = ['blk12', 'blk12', 'blk12', 'blk2 imp', 'phdnews_txt fr', 'important-news', 'p_middle']  # 链接在html中div的类别
+    # image_class_sina = ['img_wrapper', 'img_wrapper', 'img_wrapper', 'img_wrapper', 'img_wrapper', 'img_wrapper',
+    #                     'img_wrapper']  # 图片在html中div的类
+    # for i in range(len(news_urls_sina)):
+    #     Bugger(news_urls_sina[i], classfications_sina[i], sources_sina[i], news_class_sina[i], image_class_sina[i])
+
+    news_urls_163 = ['http://news.163.com/shehui/', 'http://news.163.com/domestic/', 'http://news.163.com/world/',
+                     'http://war.163.com/', 'http://sports.163.com/', 'http://ent.163.com/',
+                     'http://news.163.com/']  # 新闻链接
+    classfications_163 = ['social', 'domestic', 'international', 'military', 'sports', 'amusement', 'hot']  # 分类
+    sources_163 = ['163', '163', '163', '163', '163', '163', '163']  # 新闻来源
+    news_class_163 = ['area-left', 'area-left', 'img', 'area-main', 'mod_bd', 'news_textList_top',
+                   'ns-mr60']  # 链接在html中div的id
+    image_class_163 = ['f_center', 'f_center', 'f_center', 'f_center', 'f_center', 'f_center', 'f_center']  # 图片在html中div的类,今日头条不用这个
+    for i in range(len(news_urls_163)):
+        Bugger(news_urls_163[i], classfications_163[i], sources_163[i], news_class_163[i], image_class_163[i])
