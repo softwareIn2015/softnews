9e7306aa5c50ac9306dcad1139d4a98bd389c6ce
Raitoxz <2560804242@qq.com>
2015-12-24 14:07:11 +0800

    现在可以整合新闻了

--- Bug.py
+++ Bug.py
@@ -6,21 +6,38 @@ import MySQLdb
 
 
 def get_html(my_url):
+    """
+    获得网页的html
+    :param my_url: 网页链接
+    :return:
+    """
     page = urllib.urlopen(my_url)
     my_html = page.read()
     return my_html
 
 
-def delete(cur):
-    # 删除数据
+def delete():
+    """
+    删除数据
+    :return:
+    """
+    conn = MySQLdb.connect(host='localhost', user='root', passwd='24678', port=3306)
+    cur = conn.cursor()
+    conn.select_db('newsdb')
     cur.execute('SET foreign_key_checks=0')
     cur.execute('truncate table news_urlsmodel')
     cur.execute('truncate table news_newsmodel')
     cur.execute('SET foreign_key_checks=1')
+    cur.close()
+    conn.close()
 
 
 def show_data(cur):
-    # 查看数据库中的数据
+    """
+    查看数据库中的数据
+    :param cur: 数据库操作的cursor
+    :return:
+    """
     cur.execute('select * from news_urlsmodel')
     urls = cur.fetchall()
     for url in urls:
@@ -30,6 +47,35 @@ def show_data(cur):
     news = cur.fetchall()
     for new in news:
         print new
+
+
+def merge(cur, title_, classfication_):
+    """
+    整合新闻
+    :param cur: 数据库操作的cursor
+    :param title_: 需要整合的新闻的链接
+    :param classfication_:需要整合的新闻的分类
+    :return:
+    """
+    cur.execute('SET NAMES utf8;')
+    cur.execute('select * from news_newsmodel where Classification = %s', classfication_)
+    news_set = cur.fetchall()  # 所有和需要整合的新闻类别相同的新闻的集合
+    title_set = title_.split()  # 需要整合的新闻的标题出现的字的集合
+    # print 'title'
+    # print title_.decode('utf-8').encode('gbk', 'ignore')
+    for news_ in news_set:
+        # print news_[1].decode('utf-8').encode('gbk', 'ignore')
+        news_title_set = news_[1].split()  # 数据库中的新闻的标题出现的字的集合
+        count = 0
+        for item in title_set:
+            if item in news_title_set:
+                count += 1
+        similarity = 1.0 * count / len(title_set)
+        if similarity > 0.5:
+            print "return"
+            print news_
+            return news_, similarity  # 直接返回这个新闻和相似度
+    return None, 0
 
 
 class Bugger:
@@ -48,10 +94,13 @@ class Bugger:
         self.image_class = image_class_
         if self.source == 'sina':
             self.scripy_sina()
+            print 'sina'
         elif self.source == '163':
             self.scripy_163()
+            print '163'
         elif self.source == 'sohu':
             self.scripy_sohu()
+            print 'sohu'
 
     def scripy_sina(self):
         try:
@@ -73,6 +122,20 @@ class Bugger:
                     # 获取标题和链接
                     title = unicode(link.string).strip().encode('utf-8')  # .replace(u'\xa0', '')  # \xa0是HTML里的&nbsp
                     urls = unicode(link.get('href')).strip().encode('utf-8')
+
+                    # 整合新闻
+                    similar_news, similarity = merge(cur, title, self.classification)
+                    if similar_news is not None:  # 非空表示新闻可以整合，为空表示没有相似的新闻，直接执行后面的
+                        if similarity == 1.0:  # 新闻重复了
+                            continue
+                        else:  # 整合新闻
+                            print 'cao'
+                            print similar_news[1].decode('utf-8').encode('gbk', 'ignore')
+                            print 'cao'
+                            url_value = [self.source, urls, similar_news]
+                            cur.execute('insert into news_urlsmodel(Source, Urls, News_id) values (%s,%s,%s)',
+                                        url_value)
+                            continue
 
                     # 根据得到的url爬取新闻的概要,如果能爬到新闻的概要才存到数据库中
                     detail_url = urls
@@ -126,9 +189,6 @@ class Bugger:
                     cur.execute('insert into news_urlsmodel(Source, Urls, News_id) values (%s,%s,%s)', url_value)
                     conn.commit()
 
-            # # 删除数据
-            # delete(self.cur)
-
             cur.close()
             conn.close()
 
@@ -158,6 +218,19 @@ class Bugger:
                 # 获取标题和链接
                 title = unicode(link.string).strip().encode('utf-8')  # .replace(u'\xa0', '')  # \xa0是HTML里的&nbsp
                 urls = unicode(link.get('href')).strip().encode('utf-8')
+
+                # 整合新闻
+                similar_news, similarity = merge(cur, title, self.classification)
+                if similar_news is not None:  # 非空表示新闻可以整合，为空表示没有相似的新闻，直接执行后面的
+                    if similarity == 1.0:  # 新闻重复了
+                        continue
+                    else:  # 整合新闻
+                        print 'shit'
+                        print similar_news[1].decode('utf-8').encode('gbk', 'ignore')
+                        print 'shit'
+                        url_value = [self.source, urls, similar_news]
+                        cur.execute('insert into news_urlsmodel(Source, Urls, News_id) values (%s,%s,%s)', url_value)
+                        continue
 
                 # 根据得到的url爬取新闻的概要,如果能爬到新闻的概要才存到数据库中
                 detail_url = urls
@@ -221,9 +294,6 @@ class Bugger:
                 cur.execute('insert into news_urlsmodel(Source, Urls, News_id) values (%s,%s,%s)', url_value)
                 conn.commit()
 
-            # # 删除数据
-            # delete(cur)
-
             cur.close()
             conn.close()
 
@@ -252,13 +322,25 @@ class Bugger:
 
                 # 获取标题和链接
                 title = unicode(link.string).strip().encode('utf-8')  # .replace(u'\xa0', '')  # \xa0是HTML里的&nbsp
+
+                if len(title) <= 14:  # 爬取的时候会出现”阅读全文》“这个东西，有点坑爹
+                    continue
+
                 urls = unicode(link.get('href')).strip().encode('utf-8')
 
-                # # 判断是否重复
-                # cur.execute('select * from news_newsmodel where title = %s', title)
-                # news_exist = cur.fetchone()
-                # if len(news_exist) == 1:  # 已经存在
-                #     continue
+                # 整合新闻
+                similar_news, similarity = merge(cur, title, self.classification)
+                if similar_news is not None:  # 非空表示新闻可以整合，为空表示没有相似的新闻，直接执行后面的
+                    if similarity == 1.0:  # 新闻重复了
+                        print "cao"
+                        print similar_news[1].decode('utf-8').encode('gbk', 'ignore')
+                        continue
+                    else:  # 整合新闻
+                        print "fuck"
+                        print similar_news[1].decode('utf-8').encode('gbk', 'ignore')
+                        url_value = [self.source, urls, similar_news]
+                        cur.execute('insert into news_urlsmodel(Source, Urls, News_id) values (%s,%s,%s)', url_value)
+                        continue
 
                 # 根据得到的url爬取新闻的概要,如果能爬到新闻的概要才存到数据库中
                 detail_url = urls
@@ -320,9 +402,6 @@ class Bugger:
                 cur.execute('insert into news_urlsmodel(Source, Urls, News_id) values (%s,%s,%s)', url_value)
                 conn.commit()
 
-            # # 删除数据
-            # delete(cur)
-
             cur.close()
             conn.close()
 
@@ -334,39 +413,43 @@ class Bugger:
 
 
 if __name__ == '__main__':
-    # # 新浪的信息
-    # news_urls_sina = ['http://news.sina.com.cn/society/', 'http://news.sina.com.cn/china/',
-    #                   'http://news.sina.com.cn/world/','http://mil.news.sina.com.cn/', 'http://sports.sina.com.cn/',
-    #                   'http://ent.sina.com.cn/','http://news.sina.com.cn/']  # 新闻链接
-    # classfications_sina = ['social', 'domestic', 'international', 'military', 'sports', 'amusement', 'hot']  # 分类
-    # sources_sina = ['sina', 'sina', 'sina', 'sina', 'sina', 'sina', 'sina']  # 新闻来源
-    # # 链接在html中div的类别
-    # news_class_sina = ['blk12', 'blk12', 'blk12', 'blk2 imp', 'phdnews_txt fr', 'important-news', 'p_middle']
-    # image_class_sina = ['img_wrapper', 'img_wrapper', 'img_wrapper', 'img_wrapper', 'img_wrapper', 'img_wrapper',
-    #                     'img_wrapper']  # 图片在html中div的类
-    # for i in range(len(news_urls_sina)):
-    #     Bugger(news_urls_sina[i], classfications_sina[i], sources_sina[i], news_class_sina[i], image_class_sina[i])
+    # 先将数据库中的东西删掉
+    delete()
 
-    # # 网易的信息
-    # news_urls_163 = ['http://news.163.com/shehui/', 'http://news.163.com/domestic/', 'http://news.163.com/world/',
-    #                  'http://war.163.com/', 'http://sports.163.com/', 'http://ent.163.com/',
-    #                  'http://news.163.com/']  # 新闻链接
-    # classfications_163 = ['social', 'domestic', 'international', 'military', 'sports', 'amusement', 'hot']  # 分类
-    # sources_163 = ['163', '163', '163', '163', '163', '163', '163']  # 新闻来源
-    # # 链接在html中div的id
-    # news_class_163 = ['area-left', 'area-left', 'img', 'area-main', 'mod_bd', 'news_textList_top','ns-mr60']
-    # image_class_163 = ['f_center', 'f_center', 'f_center', 'f_center', 'f_center', 'f_center', 'f_center']
-    # for i in range(len(news_urls_163)):
-    #     Bugger(news_urls_163[i], classfications_163[i], sources_163[i], news_class_163[i], image_class_163[i])
+    # 新浪的信息
+    news_urls_sina = ['http://news.sina.com.cn/society/', 'http://news.sina.com.cn/china/',
+                      'http://news.sina.com.cn/world/', 'http://mil.news.sina.com.cn/', 'http://sports.sina.com.cn/',
+                      'http://ent.sina.com.cn/', 'http://news.sina.com.cn/']  # 新闻链接
+    classfications_sina = ['social', 'domestic', 'international', 'military', 'sports', 'amusement', 'hot']  # 分类
+    sources_sina = ['sina', 'sina', 'sina', 'sina', 'sina', 'sina', 'sina']  # 新闻来源
+    # 链接在html中div的类别
+    news_class_sina = ['blk12', 'blk12', 'blk12', 'blk2 imp', 'phdnews_txt fr', 'important-news', 'p_middle']
+    image_class_sina = ['img_wrapper', 'img_wrapper', 'img_wrapper', 'img_wrapper', 'img_wrapper', 'img_wrapper',
+                        'img_wrapper']  # 图片在html中div的类
+    for i in range(len(news_urls_sina)):
+        Bugger(news_urls_sina[i], classfications_sina[i], sources_sina[i], news_class_sina[i], image_class_sina[i])
+
+    # 网易的信息
+    news_urls_163 = ['http://news.163.com/shehui/', 'http://news.163.com/domestic/', 'http://news.163.com/world/',
+                     'http://war.163.com/', 'http://sports.163.com/', 'http://ent.163.com/',
+                     'http://news.163.com/']  # 新闻链接
+    classfications_163 = ['social', 'domestic', 'international', 'military', 'sports', 'amusement', 'hot']  # 分类
+    sources_163 = ['163', '163', '163', '163', '163', '163', '163']  # 新闻来源
+    # 链接在html中div的id
+    news_class_163 = ['area-left', 'area-left', 'img', 'area-main', 'mod_bd', 'news_textList_top', 'ns-mr60']
+    # 图片在html中div的类
+    image_class_163 = ['f_center', 'f_center', 'f_center', 'f_center', 'f_center', 'f_center', 'f_center']
+    for i in range(len(news_urls_163)):
+        Bugger(news_urls_163[i], classfications_163[i], sources_163[i], news_class_163[i], image_class_163[i])
 
     # 搜狐的信息
-    news_urls_sohu = ['http://news.sohu.com/shehuixinwen.shtml', 'http://news.sohu.com/guoneixinwen.shtml', 'http://news.sohu.com/guojixinwen.shtml',
-                     'http://mil.sohu.com/', 'http://sports.sohu.com/', 'http://cul.sohu.com/culturenews/',
-                     'http://news.sohu.com/']  # 新闻链接
+    news_urls_sohu = ['http://news.sohu.com/shehuixinwen.shtml', 'http://news.sohu.com/guoneixinwen.shtml',
+                      'http://news.sohu.com/guojixinwen.shtml', 'http://mil.sohu.com/', 'http://sports.sohu.com/',
+                      'http://cul.sohu.com/culturenews/', 'http://news.sohu.com/']  # 新闻链接
     classfications_sohu = ['social', 'domestic', 'international', 'military', 'sports', 'amusement', 'hot']  # 分类
     sources_sohu = ['sohu', 'sohu', 'sohu', 'sohu', 'sohu', 'sohu', 'sohu']  # 新闻来源
     news_class_sohu = ['new-article', 'new-article', 'new-article', 'c-m-r', 'center', 'f14list', 'r']
-    # 链接在html中div的class c-m-r center万一变了就坑爹了
-    image_class_sohu = ['', '', '', '', '', '', '']  # 图片在html中div的类,今日头条不用这个
+    # 链接在html中div的class c-m-r center f14list r 万一变了就坑爹了
+    image_class_sohu = ['', '', '', '', '', '', '']  # 图片在html中div的类,搜狐不用这个
     for i in range(len(news_urls_sohu)):
         Bugger(news_urls_sohu[i], classfications_sohu[i], sources_sohu[i], news_class_sohu[i], image_class_sohu[i])
